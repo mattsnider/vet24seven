48c19b5d8faef193160dab2d3dfdfa28757f83eb
mattsnider <mattesnider@gmail.com>
2013-01-10 18:53:55 -0800

    adding social interaction and scroll from about the app

--- homepage/static/js/home.js
+++ homepage/static/js/home.js
@@ -1,3 +1,5 @@
+var isAnimating = false;
+
 $('form').submit(function(e) {
     var elForm = $(e.target);
     e.preventDefault();
@@ -7,4 +9,28 @@ $('form').submit(function(e) {
     });
     elForm.find('button').removeClass('on').addClass('off');
     elForm.find('input[type=text]').val('').focus();
+});
+
+$('a.link-back').click(function(e) {
+    e.preventDefault();
+
+    if (!isAnimating) {
+        var elTarget = $(e.target),
+            elScrollTarget = $('.scroll_target'),
+            iStepSize = Math.round((elScrollTarget.offset().top - elTarget.offset().top) / 25),
+            iStepIndex = 0,
+            iIntervalId = setInterval(function() {
+                if (5 > iStepIndex) {
+                    window.scrollTo(0, iStepSize * iStepIndex * iStepIndex);
+                    iStepIndex += 1;
+                }
+                else {
+                    window.scrollTo(0, elScrollTarget.offset().top);
+                    clearInterval(iIntervalId);
+                    isAnimating = false;
+                }
+            }, 100);
+
+        isAnimating = true;
+    }
 });
