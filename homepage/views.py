375553ae6d8ea7f4f9d34aad7ab657ed3c0fb3a2
matt snider <mattesnider@gmail.com>
2013-01-13 13:46:48 -0800

    caching the homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.decorators.cache import cache_page
 from django.views.decorators.http import require_POST
 from django_shared.http import JsonResponse
 
 from homepage.forms import EmailForm
 from homepage.models import CAMPAIGNS, MEDIUM, SOURCE
 
+CACHE_LENGTH_HOUR = 60 * 60
+CACHE_LENGTH_DAY = CACHE_LENGTH_HOUR * 24
 
+
+@cache_page(CACHE_LENGTH_DAY * 7)
 def index(request):
     """
     Shows the homepage.
